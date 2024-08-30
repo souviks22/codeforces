f18a29f94eda90b5c2c468043889b0bf96989add
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-08-30 22:46:54 +0530

    Codeforces Round 969 A

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from sys import stdout
 for _ in range(int(input())):
-    n = int(input())
-    edges = []
-    marked = [False]*(n+1)
-    def find(a,b):
-        if marked[a] and marked[b]:
-            return
-        print(f'? {a} {b}')
-        stdout.flush()
-        x = int(input())
-        if x == a:
-            edges.append(a)
-            edges.append(b)
+    l, r = map(int,input().split())
+    cur, ops = l, 0
+    while cur <= r-2:
+        if cur % 2 == 0:
+            cur += 1
         else:
-            find(a,x)
-            find(x,b)
-        marked[a] = marked[b] = True
-
-    for v in range(2,n+1):
-        if not marked[v]:
-            find(1,v)
-    print('!',end=' ')
-    print(*edges)    
+            cur += 3
+            ops += 1
+    print(ops)
