750b3455f848015a2864fa29b09c4aae0d82a270
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-10-06 00:23:30 +0530

    Meta Hacker Cup: Prime Subfactorization

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 import sys
-sys.stdin = open('subsonic_subway_input.txt','r')
+sys.stdin = open('prime_subtractorization_input.txt','r')
 for i in range(int(input())):
     n = int(input())
-    a = [tuple(map(int,input().split()) )for _ in range(n)]
-    def pos(speed):
-        for i, (start, end) in enumerate(a):
-            cur = (i+1)/speed
-            if cur < start:
-                return -1
-            if cur > end:
-                return 1
-        return 0
-    lo, hi, res = 0, 10**18, -1
-    while True:
-        mid = int(((lo+hi)/2)*10**6)/10**6
-        if mid == lo or mid == hi:
-            break
-        cur = pos(mid)
-        if cur == -1:
-            hi = mid
-        elif cur == 1:
-            lo = mid
-        else:
-            res = mid
-            hi = mid
-    print(f'Case #{i+1}: {res}')
+    prime = [True]*(n+1)
+    prime[0] = prime[1] = False
+    cur = 2
+    while cur*cur <= n:
+        if prime[cur]:
+            for num in range(cur*cur,n+1,cur):
+                prime[num] = False
+        cur += 1
+    count = int(n >= 5)
+    for num in range(n+1):
+        count += prime[num] and num+2 <= n and prime[num+2]
+    print(f'Case #{i+1}: {count}')
