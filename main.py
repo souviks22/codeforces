3dceab246d32d4e9f9f3d9c880e25c7556b100d8
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-10-05 23:31:14 +0530

    Meta Hacker Cup: Subsonic Subway

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import math
-for _ in range(int(input())):
-    n, a, b = map(int, input().split())
-    gcd = math.gcd(a, b)
-    arr = list(map(int, input().split()))
-    for i in range(n):
-        arr[i] %= gcd
-    arr.sort()
-    mini = arr[-1] - arr[0]
-    for i in range(n - 1):
-        mini = min(mini, arr[i] + gcd - arr[i + 1])
-    print(mini)
+import sys
+sys.stdin = open('subsonic_subway_input.txt','r')
+for i in range(int(input())):
+    n = int(input())
+    a = [tuple(map(int,input().split()) )for _ in range(n)]
+    def pos(speed):
+        for i, (start, end) in enumerate(a):
+            cur = (i+1)/speed
+            if cur < start:
+                return -1
+            if cur > end:
+                return 1
+        return 0
+    lo, hi, res = 0, 10**18, -1
+    while True:
+        mid = int(((lo+hi)/2)*10**6)/10**6
+        if mid == lo or mid == hi:
+            break
+        cur = pos(mid)
+        if cur == -1:
+            hi = mid
+        elif cur == 1:
+            lo = mid
+        else:
+            res = mid
+            hi = mid
+    print(f'Case #{i+1}: {res}')
