f8f63942c2e2493a41452ad956bf7a991a8aba83
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-10-09 00:34:30 +0530

    Nearest Meeting Cell

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import sys
-sys.stdin = open('prime_subtractorization_input.txt','r')
-for i in range(int(input())):
+for _ in range(int(input())):
     n = int(input())
-    prime = [True]*(n+1)
-    prime[0] = prime[1] = False
-    cur = 2
-    while cur*cur <= n:
-        if prime[cur]:
-            for num in range(cur*cur,n+1,cur):
-                prime[num] = False
-        cur += 1
-    count = int(n >= 5)
-    for num in range(n+1):
-        count += prime[num] and num+2 <= n and prime[num+2]
-    print(f'Case #{i+1}: {count}')
+    child = list(map(int,input().split()))
+    c1, c2 = map(int,input().split())
+    def get_order(c):
+        order = [-1]*n
+        order[c] = 0
+        while child[c] != -1 and order[child[c]] == -1:
+            order[child[c]] = 1 + order[c]
+            c = child[c]
+        return order
+    o1, o2 = get_order(c1), get_order(c2)
+    cm, mini = -1, n+1
+    for i in range(n):
+        if o1[i] == -1 or o2[i] == -1:
+            continue
+        level = max(o1[i],o2[i])
+        if level < mini:
+            cm = i
+            mini = level
+    print(cm)
